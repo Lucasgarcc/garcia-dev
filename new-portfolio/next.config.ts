656057d03bd769faee3b9facad8c0d48a58a1d56
Lucasgarcc <luuccasgarciia@gmail.com>
2025-06-15 17:39:29 -0300

    refactor: :recycle: uso do logo.svg como componente + ajustes de compatibilidade
    - Removido turbo do script dev no package.json para evitar conflitos com SVG como componente.
    - Atualizado next.config.ts para suportar importação de SVG via @svgr/webpack.
    - Movida pasta assets de public para src/assets para permitir importações dinâmicas.
    - Adicionado suporte a temas dinâmicos via variáveis CSS.
    - Estilização do logo agora controlada diretamente pelo Header e CSS Modules.
    - Build gerado com sucesso após alterações.

--- new-portfolio/next.config.ts
+++ new-portfolio/next.config.ts
@@ -1,7 +1,55 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  experimental: {
+    forceSwcTransforms: true,
+  },
+
+    webpack(config: any) {
+    // Encontra a regra que carrega arquivos que contenham SVG, protegendo o acesso a test
+    const fileLoaderRule = config.module.rules.find(
+        (rule: any) => rule.test instanceof RegExp && rule.test.test('.svg')
+    );
+
+    if (fileLoaderRule) {
+        fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                icon: true,
+                svgo: true,
+                svgoConfig: {
+                    plugins: [
+                        {
+                            name: 'removeViewBox',
+                            active: false,
+                        },
+                    ],
+                    },
+                    svgProps: {
+                        fill: 'currentColor',
+                        Stroke: 'currentColor',
+                        stop: {
+                            stopColor: 'currentColor',
+                            offset: '0%' 
+                        },
+                    },
+                },
+            },
+        ],
+    });
+
+    return config;
+    }
+
 };
 
 export default nextConfig;
--- new-portfolio/src/app/components/Home/Header/Header.tsx
+++ new-portfolio/src/app/components/Home/Header/Header.tsx
@@ -1,7 +1,8 @@
 'use client';
+import '../../../globals.css';
 import { AppBar, Toolbar, Typography } from '@mui/material';
-import logo from '../../../../../public/assets/logo 2.svg';
-import Image from 'next/image';
+import Logo from '@/assets/logo.svg';
+//import Image from 'next/image';
 import HeaderStyle from './Header.module.css';
 
 const Header = () => {
@@ -10,7 +11,11 @@ const Header = () => {
         <AppBar sx={{ backgroundColor: 'var(--bg-header)', color: 'var(--color-header)', boxShadow: 'none' }} position="static">
             <Toolbar >
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <Image src={logo} alt="Logo" width={100} height={100} className={HeaderStyle.logo}/>
+                    <Logo  
+                        alt="Logo" width={100}
+                        height={100} 
+                        className={`${HeaderStyle.logo} ${HeaderStyle.path1} ${HeaderStyle.path2} ${HeaderStyle.path3} ${HeaderStyle.paint_linear} `} 
+                    />
                 </Typography>
             </Toolbar>
         </AppBar>
