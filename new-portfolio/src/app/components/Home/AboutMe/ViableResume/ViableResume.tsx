1d67c17a8e1a52f22ce26efffc641c79f069f18d
Lucasgarcc <dev2@f5sg.com.br>
2025-10-04 18:13:02 -0300

    feat: :sparkles: Adiciona o componente `ViableResume` e ajusta o assets para public

---
+++ new-portfolio/src/app/components/Home/AboutMe/ViableResume/ViableResume.tsx
@@ -0,0 +1,38 @@
+'client'
+
+import React from 'react';
+import '../../../../globals.css';
+import Styles from './ViableResume.module.css';
+import { Button } from '@mui/material';
+import { BsDownload } from "react-icons/bs";
+const ViableResume = () => {
+    return (
+        <div>
+            <Button
+                component="a" // transforma o botão em link
+                href="/assets/LucasGarcia-DesenvolvedorFrontend.pdf" // arquivo na pasta public
+                download
+                target="_blank"
+                variant="contained"
+                sx={{
+                    backgroundColor: 'var(--primary-c-3)',
+                    marginTop: '1rem',
+                    padding: '1.1rem',
+                    color: 'var(--color-text-n)',
+                    ":hover": {
+                        backgroundColor: "var(--primary-c-2)",
+                        transform: "translateY(-2px)",
+                    },
+                    transition: "all 0.3s ease",
+                    textTransform: 'none'
+                }}
+                startIcon={<BsDownload />}
+                aria-label="Baixar CV"
+            >
+                Baixar CV
+            </Button>
+        </div>
+    );
+}
+
+export default ViableResume;
